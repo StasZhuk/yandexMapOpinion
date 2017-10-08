371ce9d7bb3b94febfc543afe17ec5ee919db372
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-10-08 16:31:21 +0300

    change baloon layout

--- src/index.js
+++ src/index.js
@@ -28,70 +28,58 @@ function getAddres(coords) {
 
 let myMap;
 let clusterer;
-let flag = 1;
-let template = '<div class="opinion__header">' +
-                    '<div class="opinion__marker"></div>' +
-                    '<div class="opinion__adress"></div>' +
-                    '<div class="btn-exit"></div>' +
-                '</div>' +
-                '<div class="opinion__body">' +
-                    '<div class="opinion__comment-outer"><span class="no-comment">Отзывов пока нет...<span></div>' +
-                    '<div class="opinion__title">Ваш отзыв</div>' +
-                    '<div class="input-wrap">' +
-                        '<input id="" type="text" name="name" placeholder="Ваше имя">' +
-                    '</div>' +
-                    '<div class="input-wrap">' +
-                        '<input type="text" name="place" placeholder="Укажите место">' +
-                    '</div>' +
-                    '<div class="input-wrap">' +
-                        '<textarea name="comment" cols="30" rows="5" placeholder="Поделитесь впечатлениями"></textarea>' +
-                    '</div>' +
-                    '<div class="btn__wrap">' +
-                        '<div class="btn-add">Добавить</div>' +
-                    '</div>' +
-                '</div>';
+let points = [];
+// let geoObjects = [];
+
 
 new Promise(resolve => ymaps.ready(resolve))
     .then(() => mapInit())
     .then(() => Clusterer())
     .then(() => {
         myMap.events.add('click', function (e) {
-            if (window.opinion) {
-                window.opinion.parentElement.removeChild(window.opinion);
-
-                return false;
-            }
 
             let coords = e.get('coords');
             let addres = getAddres(coords);
 
             addres
                 .then(pointName => {
-                    let div = document.createElement('div');
+                    let template = '<div class="opinion popover">' +
+                                        '<div class="opinion__header">' +
+                                            '<div class="opinion__marker"></div>' +
+                                            '<div class="opinion__adress">' + pointName + '</div>' +
+                                        '</div>' +
+                                        '<div class="opinion__body">' +
+                                            '<div class="opinion__comment-outer"><span class="no-comment">Отзывов пока нет...<span></div>' +
+                                            '<div class="opinion__title">Ваш отзыв</div>' +
+                                            '<div class="input-wrap">' +
+                                                '<input id="" type="text" name="name" placeholder="Ваше имя">' +
+                                            '</div>' +
+                                            '<div class="input-wrap">' +
+                                                '<input type="text" name="place" placeholder="Укажите место">' +
+                                            '</div>' +
+                                            '<div class="input-wrap">' +
+                                                '<textarea name="comment" cols="30" rows="5" placeholder="Поделитесь впечатлениями"></textarea>' +
+                                            '</div>' +
+                                            '<div class="btn__wrap">' +
+                                                '<div class="btn-add">Добавить</div>' +
+                                            '</div>' +
+                                        '</div>' +
+                                    '</div>';
 
-                    div.classList.add('opinion');
-                    div.setAttribute('id', 'opinion');
-                    div.innerHTML = template;
+                    // Создание метки с пользовательским макетом балуна.
+                    if (!myMap.balloon.isOpen()) {
+                        myMap.balloon.open(coords, {
+                            contentBody: template
+                        });
 
-                    map.appendChild(div);
-
-                    let opinion = document.getElementById('opinion'),
-                        mouseOffsetTop = e.get('pagePixels')[1],
-                        mouseOffsetLeft = e.get('pagePixels')[0];
-                    
-                    if (opinion) {
-                        opinion.style.left = mouseOffsetLeft + 'px';
-                        opinion.style.top = mouseOffsetTop + 'px';
-                        opinion.querySelector('.opinion__adress').innerText = pointName;
-
-                        opinion.addEventListener('click', function(e) {
+                        document.body.addEventListener('click', function(e) {
                             if (e.target.classList.contains('btn-add')) {
+                                let contOpinion = e.target.parentElement.parentElement.parentElement; 
                                 let commentInput = e.target.parentElement.previousElementSibling.children[0];
                                 let placeInput = commentInput.parentElement.previousElementSibling.children[0];
                                 let nameInput = placeInput.parentElement.previousElementSibling.children[0];
                                 let commentOut = nameInput.parentElement.previousElementSibling.previousElementSibling;
-                                let templateBaloon;
-                                let BalloonLayout;
+                                let baloonTemplate;
                                 
                                 if (commentInput.value !== '' && placeInput.value !== '' && nameInput.value !== '') {
                                     let obj = {
@@ -101,92 +89,25 @@ new Promise(resolve => ymaps.ready(resolve))
                                     };
                                     let div = document.createElement('div');
 
-                                    if (commentOut.firstElementChild.tagName == 'SPAN') {
+                                    if (commentOut.firstElementChild.classList.contains('no-comment')) {
                                         commentOut.innerHTML = '';
                                     }
 
                                     div.innerHTML = `<div><span>${obj.name}</span><span> ${obj.place}</span></div><div>${obj.comment}</div>`;
 
                                     commentOut.appendChild(div);
-                                    opinion.setAttribute('id', 'opinion-' + flag);
 
-                                    templateBaloon = opinion.outerHTML;
-                                    opinion.setAttribute('id', 'opinion');
-                                    
-                                    window.opinion.parentElement.removeChild(window.opinion);
+                                    commentInput.value = '';
+                                    placeInput.value = '';
+                                    nameInput.value = '';
 
-                                    // создаем поьзовательский макет в балуне
-                                    BalloonLayout = ymaps.templateLayoutFactory.createClass(templateBaloon, {
-                                        /**
-                                             * Строит экземпляр макета на основе шаблона и добавляет его в родительский HTML-элемент.
-                                             * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#build
-                                             * @function
-                                             * @name build
-                                             */
-                                        build: function () {
-                                            this.constructor.superclass.build.call(this);
-                            
-                                            this._$element = $('.popover', this.getParentElement());
-                            
-                                            this.applyElementOffset();
-                            
-                                            this._$element.find('.close')
-                                                .on('click', $.proxy(this.onCloseClick, this));
-                                        },
-                            
-                                        /**
-                                             * Удаляет содержимое макета из DOM.
-                                             * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#clear
-                                             * @function
-                                             * @name clear
-                                             */
-                                        clear: function () {
-                                            this._$element.find('.close')
-                                                .off('click');
-                            
-                                            this.constructor.superclass.clear.call(this);
-                                        },
-                                        /**
-                                             * Сдвигаем балун, чтобы "хвостик" указывал на точку привязки.
-                                             * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
-                                             * @function
-                                             * @name applyElementOffset
-                                             */
-                                        applyElementOffset: function () {
-                                            this._$element.css({
-                                                left: -(this._$element[0].offsetWidth / 2),
-                                                top: -(this._$element[0].offsetHeight + this._$element.find('.btn-add')[0].offsetHeight)
-                                            });
-                                        },
-                                        /**
-                                             * Используется для автопозиционирования (balloonAutoPan).
-                                             * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ILayout.xml#getClientBounds
-                                             * @function
-                                             * @name getClientBounds
-                                             * @returns {Number[][]} Координаты левого верхнего и правого нижнего углов шаблона относительно точки привязки.
-                                             */
-                                        getShape: function () {
-                                            if(!this._isElement(this._$element)) {
-                                                return BalloonLayout.superclass.getShape.call(this);
-                                            }
-                    
-                                            var position = this._$element.position();
-                    
-                                            return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
-                                                [position.left, position.top], [
-                                                    position.left + this._$element[0].offsetWidth,
-                                                    position.top + this._$element[0].offsetHeight + this._$element.find('.btn-add')[0].offsetHeight
-                                                ]
-                                            ]));
-                                        },
-                                    });
-                                    
-                                // Создание метки с пользовательским макетом балуна.
-                                    myPlacemark = window.myPlacemark = new ymaps.Placemark(coords, {
-                                        // balloonContent: '<div>Привет</div>'
+                                    baloonTemplate = contOpinion.outerHTML;
+
+                                    var myPlacemark = new ymaps.Placemark(coords, {
+                                        balloonContent: '<div>привет' + obj.name + '</div>'
                                     }, {
                                         balloonShadow: false,
-                                        balloonLayout: BalloonLayout,
+                                        // balloonContentLayout: BalloonLayout,
                                         balloonPanelMaxMapArea: 0,
                                         // Не скрываем иконку при открытом балуне.
                                         hideIconOnBalloonOpen: false,
@@ -195,15 +116,16 @@ new Promise(resolve => ymaps.ready(resolve))
                                     });
                         
                                     myMap.geoObjects.add(myPlacemark);
-
-                                    ++flag;
                                 }
                             }
                         });
+                        
+                    } else {
+                        myMap.balloon.close();
                     }
                 })
-        })
-    });
+        });
+    })
     // .catch(() => {
     //     console.log('ошибка');
     // })
